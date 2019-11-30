62a8497d36c16980a026bc2827b1d1c352ee0c85
Henry Medina <elpadredelcordero@gmail.com>
2019-11-30 01:10:11 -0500

    busqueda por alumnos terminado

--- buscador.py
+++ buscador.py
@@ -8,10 +8,15 @@ class Buscador:
         conexion=sqlite3.connect("orce.db")
         self.cursor=conexion.cursor()
     def buscar_alumno(self,codigo):
-        pass
+        self.cursor.execute("select * from notas_industriales where codigo=?",[codigo.upper()])
+        resultados1=self.cursor.fetchall()
+        self.cursor.execute("select * from alumnos where codigo=?",[codigo.upper()])
+        resultados2=self.cursor.fetchall()
+        self.cursor.execute("select * from notas_sistemas where codigo=?",[codigo.upper()])
+        resultados3=self.cursor.fetchall()
+        return resultados1, resultados2,resultados3
 
     def buscar_curso(self, codigo):
-
         self.cursor.execute("select * from cursos where codigo=?",[codigo.upper()])
         resultados=self.cursor.fetchall()
         return resultados
--- main.py
+++ main.py
@@ -4,6 +4,8 @@ if __name__=="__main__":
     usar=True
     buscar=Buscador()
     while usar:
+        print()
+        print()
         print("Bienvenidos al Sistema de Información \n Elija una opción:\n a) Buscar Curso por Código\n b)Buscar alumno por código\n c)Salir")
         buscar.conectar()
         eleccion=input()
@@ -12,11 +14,33 @@ if __name__=="__main__":
             usar=False
         elif eleccion=="a":
             codigo=input("Ingrese el código del curso")
-            resultados=buscar.buscar_curso(codigo)
-            for resultado in resultados:
-                print("curso:"+ resultado[0]+" código: "+resultado[1]+" créditos: "+str(resultado[2]))
-
+            cursos=buscar.buscar_curso(codigo)
+            for curso in cursos:
+                print("curso:"+ curso[0]+" código: "+curso[1]+" créditos: "+str(curso[2]))
+            codigo.close()
         elif eleccion=="b":
-            print("Haz elegido la b")
+            codigo=input("Ingrese el código del alumno")
+            notas_ind,alumnos,notas_sis=buscar.buscar_alumno(codigo)
+            for alumno in alumnos:
+                print("Alumno: "+alumno[2]+" "+alumno[3]+" "+alumno[0]+" "+alumno[1]+"\n Código: "+alumno[4]+"\n Especialidad: "+alumno[5]+"\n Cursos:")
+                if alumno[5]=="industrial":
+                    for nota in notas_ind:
+                        print("Cálculo Diferencial: "+ str(nota[1]))
+                        print("Química I: "+ str(nota[2]))
+                        print("Redacción y Comunicación: "+ str(nota[3]))
+                        print("Ética y Filosofía política: "+ str(nota[4]))
+                        print("Geometría Analítica: "+ str(nota[5]))
+                        print("Dibujo en Ingeniería: "+ str(nota[6]))
+                        print("Introducción a la Ing. Industrial: "+ str(nota[7]))
+                else:
+                    for nota in notas_sis:
+                        print("Geometría Analítica: "+ str(nota[1]))
+                        print("Cálculo Diferencial: "+ str(nota[2]))
+                        print("Química I: "+ str(nota[3]))
+                        print("Introducción a la Computación: "+ str(nota[4]))
+                        print("Redacción y Comunicación: "+ str(nota[5]))
+                        print("Introducción al pensamiento y a la Ing Sistemas: "+ str(nota[6]))
+                
+            
         else:
             print("Haz elegido una opción no válida, vuelva a intentarlo")
